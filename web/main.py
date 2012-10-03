982fe9b4212c02e931dfa257014014659b1ff882
acbart <acbart@vt.edu>
2012-10-03 17:04:27 -0400

    Updates

--- web/main.py
+++ web/main.py
@@ -1,5 +1,8 @@
 from __future__ import with_statement
 
+# Low Level imports
+from optparse import OptionParser
+
 # Import database call
 #import sqlalchemy
 import sqlite3
@@ -45,5 +48,17 @@ def show_entries():
     
     
 if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-s", "--server", dest="local", default=True,
+                      action="store_false",
+                      help="Run as server (mickey.cs.vt.edu:8000 currently)")
+    parser.add_option("-l", "--local", dest="local",
+                      action="store_true", default=True,
+                      help="Run locally (127.0.0.1:5000)")
+
+    (options, args) = parser.parse_args()
     init_db()
-    app.run(host='mickey.cs.vt.edu', port=8000)
+    if options.local:
+        app.run(host='127.0.0.1', port=5000)
+    else:
+        app.run(host='mickey.cs.vt.edu', port=8000)
